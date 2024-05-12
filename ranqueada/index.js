0926aca5162b10a909c9639f5f9de97a786278e5
emerson16inacio <netoinacio82@gmail.com>
2024-05-11 21:35:53 -0300

    Calculadora de Ranqueada

---
+++ ranqueada/index.js
@@ -0,0 +1,31 @@
+function calcularNivel(vitorias, derrotas) {
+    // Cálculo do saldo de vitórias
+    const saldoVitorias = vitorias - derrotas;
+  
+    // Determinação do nível
+    let nivel;
+    if (vitorias < 10) {
+      nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+      nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+      nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+      nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+      nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+      nivel = "Lendário";
+    } else {
+      nivel = "Imortal";
+    }
+  
+    // Exibir mensagem final
+    console.log("O Herói tem um saldo de "+ saldoVitorias + " e está no nível "+ nivel + " !")
+  }
+  
+  // Exemplo de uso
+  const vitorias = 25;
+  const derrotas = 10;
+  
+  calcularNivel(vitorias, derrotas);
